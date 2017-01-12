4443f7eb5f7883008f04629b1acca326f883db91
Roy Barak <RoyB@unitronics.com>
2017-01-12 12:23:14 +0200

    test again

---
+++ ParkingDashboardSample/obj/Debug/AspnetCompileMerge/TempBuildDir/Scripts/app/services/gisSitesService.js
@@ -0,0 +1,16 @@
+﻿DashBoard.factory('GisSiteService', function ($http) {
+    var GisSiteService = {};
+    GisSiteService.GisSites = function () {
+        return $http.get('/GisSitesData/GetSites');
+    }
+
+    GisSiteService.GetSitesDetails = function (id){
+        return $http.get('/SiteData/GetSitesDetails/'+id);
+    }
+
+    GisSiteService.GetSitesEvents=function(id) {
+        return $http.get('/SiteData/GetSitesEvents/' + id);
+    }
+    return GisSiteService;
+
+});
