2c9b41a501cb82120a37b6af40c369f7e3b68310
Roy Barak <RoyB@unitronics.com>
2017-01-12 13:09:01 +0200

    local 65

---
+++ ParkingDashboardSample/Hubs/EventHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace ParkingDashboardSample.Hubs
+{
+    public class EventHub:Hub
+    {
+        public void GetEvents(string client, string message)
+        {
+            Clients.All.NewMessage(client, message);
+        }
+
+        public void Heartbeat()
+        {
+            Clients.All.heartbeat();
+        }
+
+        public override Task OnConnected()
+        {
+            return (base.OnConnected());
+        }
+    }
+}
---
+++ ParkingDashboardSample/Models/EventsData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+
+namespace ParkingDashboardSample.Models
+{
+    public class EventsData
+    {
+        public string Site { get; set; }
+        public int EventsDataId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Severity { get; set; }
+    }
+}
